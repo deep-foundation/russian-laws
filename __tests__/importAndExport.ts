1348da86207e2cd1f804561e6eeac263512f9aef
FreePhoenix888 <freephoenix888@gmail.com>
2024-02-27 16:36:21 +0600

    Add test draft

--- __tests__/importAndExport.ts
+++ __tests__/importAndExport.ts
@@ -1,3 +1,10 @@
-it('import and export', () => {
-  expect(2).toBe(2);
+import fsExtra from 'fs-extra'
+import path from 'path'
+import { processHtmlAndCreateLinks } from '../processHtmlAndCreateLinks.js';
+
+it('import and export',  async() => {
+  const filePath = path.join('..', 'data', 'html', '102041891');
+  const html = fsExtra.readFileSync(filePath, {encoding: 'utf8'});
+  const processHtmlAndCreateLinksResult = await processHtmlAndCreateLinks({html})
+  console.log({processHtmlAndCreateLinksResult})
 });
