14c67cc57ba7fbfeb12697cc6c3efe602e25c3c4
FreePhoenix888 <freephoenix888@gmail.com>
2024-03-10 16:42:08 +0000

    add log

--- html-to-links.ts
+++ html-to-links.ts
@@ -1,11 +1,13 @@
 import { DeepClient } from "@deep-foundation/deeplinks/imports/client.js";
 import { htmlToJson } from "./html-to-json.js";
 import { JsonToLinks } from "./json-to-links.js";
+import { log } from "./log.js";
 
 
 export async function htmlToLinks({deep, html ,documentLinkId}: {deep: DeepClient; html: string; documentLinkId: number }) {
 
     const json = htmlToJson({ html });
+    log({json})
 
     const jsonToLinks = await JsonToLinks.new({
         deep
